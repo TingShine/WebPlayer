ca7ca21bd4a952b916131008639500e9cd0e27d3
Liting Li <liting.li@shopee.com>
2025-02-19 15:17:18 +0800

    feat: add dot element

--- packages/render/components/progress/index.ts
+++ packages/render/components/progress/index.ts
@@ -3,6 +3,7 @@ import { Component } from "../component"
 export class ProgressBar extends Component {
   private playerBar: HTMLDivElement
   private  bufferBar: HTMLDivElement
+  private dot: HTMLDivElement
 
   constructor() {
     super()
@@ -14,6 +15,7 @@ export class ProgressBar extends Component {
     if (this.playerBar) {
       this.playerBar.style.width = `${progress}%`
       this.bufferBar.style.left = `${progress}%`
+      this.dot.style.left = `${progress}%`
     }
   }
 
@@ -38,6 +40,16 @@ export class ProgressBar extends Component {
     playerBar.style.position = "absolute"
     playerBar.style.top = "0"
     playerBar.style.left = "0"
+
+    const dot = document.createElement('div')
+    dot.style.width = "10px"
+    dot.style.height = "10px"
+    dot.style.borderRadius = "50%"
+    dot.style.backgroundColor = "white"
+    dot.style.position = "absolute"
+    dot.style.left = "0"
+    dot.style.top = "-2.5px"
+
   
     const bufferBar = document.createElement('div')
     bufferBar.style.width = "0"
@@ -50,9 +62,11 @@ export class ProgressBar extends Component {
     this.wrapper = progressBar
     this.playerBar = playerBar
     this.bufferBar = bufferBar
+    this.dot = dot
 
     progressBar.appendChild(playerBar)
     progressBar.appendChild(bufferBar)
+    progressBar.appendChild(dot)
   }
 
   private initEvent() {
@@ -67,7 +81,7 @@ export class ProgressBar extends Component {
     this.wrapper.addEventListener("click", (e) => {
       e.stopPropagation()
       e.preventDefault()
-      
+
       const rect = this.wrapper.getBoundingClientRect()
       const x = e.clientX
       const ratio = (x - rect.x) / rect.width
